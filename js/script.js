748177be2854c961b60f830d56b462e775ce9818
KareemWilson <kareemmagdywilson@gmail.com>
2022-09-19 12:09:57 +0200

    Initialize html

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const bookSection = document.querySelector('.display-books')
+const titleInput = document.querySelector('.title-input')
+const authorInput = document.querySelector('.author-input')
+const addBtn = document.querySelector('.btn-add')
+
+
+const title = document.createElement('p')
+const author = document.createElement('p')
+const removeBtn = document.createElement('button')
+
+
