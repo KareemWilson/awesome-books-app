443fccc531fc0bfe3fa9775e7a02e9bfe43ad496
KareemWilson <kareemmagdywilson@gmail.com>
2022-09-19 15:54:41 +0200

    Finish Dom Manipulation

--- js/script.js
+++ js/script.js
@@ -1,36 +1,79 @@
 const bookSection = document.querySelector(".display-books");
+const form = document.querySelector(".add-book-form");
 const titleInput = document.querySelector(".title-input");
 const authorInput = document.querySelector(".author-input");
 const addBtn = document.querySelector(".btn-add");
 
-// const form = document.querySelector('.form');
-// const library = document.querySelector('.library');
-// const Author = document.querySelector('.author');
-// const title = document.querySelector('.title');
-
-let list = JSON.parse(localStorage.books) || [];
+let list = localStorage.books ? JSON.parse(localStorage.books) : [];
 
 const getBooksFromLocalStorage = () => {
-
   if (localStorage.books) {
-  let booksArray = [];
-    booksArray = JSON.parse(localStorage.books)
-    console.log(booksArray)
-    booksArray.forEach((book) => bookSection.insertAdjacentHTML('beforeend', libraryBooks(book)));
-  } else {
-    console.log("books does not exist")
+    let booksArray = [];
+    booksArray = JSON.parse(localStorage.books);
+    booksArray.forEach((book) =>
+      bookSection.insertAdjacentHTML("beforeend", libraryBooks(book))
+    );
   }
-}
+};
 
 getBooksFromLocalStorage();
 
 function libraryBooks(object) {
-  return `<div class="${object.author}">
+  return `<div class="${object.book}">
     <h1>${object.book}</h1>
     <p>${object.author}</p>
     <hr>
     <button class="remove">
     remove
     </button>
-    </div><br><br>`;
+    </div>`;
 }
+
+const removeBtn = document.querySelector('.remove')
+
+function add() {
+  if (authorInput.value !== "" && titleInput.value !== "") {
+    const currentBook = [];
+    currentBook.push({
+      author: authorInput.value,
+      book: titleInput.value,
+    });
+    list.push({
+      author: authorInput.value,
+      book: titleInput.value,
+    });
+    if (list.length > 0) {
+      currentBook.forEach((book) =>
+        bookSection.insertAdjacentHTML("beforeend", libraryBooks(book))
+      );
+    }
+  }
+  authorInput.value = "";
+  titleInput.value = "";
+  localStorage.setItem("books", JSON.stringify(list));
+}
+
+function remove() {
+  if (list.length > 0) {
+    const removebtn = document.querySelectorAll(".remove");
+    removebtn.forEach((element) =>
+      element.addEventListener("click", function () {
+        const parentNodeClass = element.parentNode.className;
+        element.parentNode.remove();
+        list = list.filter((x) => x.book !== parentNodeClass);
+        localStorage.setItem("books", JSON.stringify(list));
+      })
+    );
+  }
+}
+
+addBtn.addEventListener('click', (e) => {
+    e.preventDefault()
+    add()
+})
+
+// form.addEventListener("submit", function (event) {
+//   event.preventDefault();
+//   add();
+//   remove();
+// });
