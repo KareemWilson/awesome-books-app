bfb2dd72e0411350c242411004311e4a19334e5d
KareemWilson <kareemmagdywilson@gmail.com>
2022-09-19 12:30:05 +0200

    Start in DOM

--- js/script.js
+++ js/script.js
@@ -1,11 +1,32 @@
-const bookSection = document.querySelector('.display-books')
-const titleInput = document.querySelector('.title-input')
-const authorInput = document.querySelector('.author-input')
-const addBtn = document.querySelector('.btn-add')
+const bookSection = document.querySelector(".display-books");
+const titleInput = document.querySelector(".title-input");
+const authorInput = document.querySelector(".author-input");
+const addBtn = document.querySelector(".btn-add");
 
+const generateBookCard = () => {
+  const title = document.createElement("p");
+  const author = document.createElement("p");
+  const removeBtn = document.createElement("button");
+  
+  title.setAttribute("class", "title");
+  author.setAttribute("class", "author");
+  removeBtn.setAttribute("class", "btn-remove");
 
-const title = document.createElement('p')
-const author = document.createElement('p')
-const removeBtn = document.createElement('button')
+  bookSection.append(title, author, removeBtn);
 
+  title.innerHTML = titleInput.value;
+  author.innerHTML = authorInput.value;
+  removeBtn.innerHTML = "Remove";
+
+  removeBtn.addEventListener('click', (e) => {
+    e.preventDefault()
+    bookSection.remove(title,author,removeBtn)
+  })
+};
+
+addBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  generateBookCard()
+  
+});
 
