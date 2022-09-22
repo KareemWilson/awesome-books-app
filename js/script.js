90c3f3920ec75dc12c31e9559b41c1c12fe9e117
KareemWilson <kareemmagdywilson@gmail.com>
2022-09-22 10:23:55 +0200

    Fix Linter Errors

--- js/script.js
+++ js/script.js
@@ -1,44 +1,31 @@
-import { Liberary } from "./class.js";
+import Liberary from './class.js';
 
 const titleInput = document.querySelector('.title-input');
 const authorInput = document.querySelector('.author-input');
 const addBtn = document.querySelector('.btn-add');
-let list = localStorage.books
-  ? JSON.parse(localStorage.getItem('books'))
-  : [];
+const list = localStorage.books ? JSON.parse(localStorage.getItem('books')) : [];
 
-window.removeBook = function removeBook(title){
-  myBookList.removeBook(title)
-  const updatedList = myBookList.fetchBooks()
-  bookSection.innerHTML = updatedList.join('');
-}
-
-const myBookList = new Liberary(list)
-console.log(myBookList);
+const myBookList = new Liberary(list);
 const bookSection = document.querySelector('.display-books');
-const booksCards = myBookList.fetchBooks()
-bookSection.innerHTML = booksCards.join('');
 
+window.removeBook = function removeBook(title) {
+  myBookList.removeBook(title);
+  const updatedList = myBookList.fetchBooks();
+  bookSection.innerHTML = updatedList.join('');
+};
+
+const booksCards = myBookList.fetchBooks();
+bookSection.innerHTML = booksCards.join('');
 
 addBtn.addEventListener('click', (e) => {
   e.preventDefault();
-  const currentBook = myBookList.createBookObj(titleInput.value, authorInput.value);
+  const currentBook = myBookList.createBookObj(
+    titleInput.value,
+    authorInput.value,
+  );
   myBookList.addBook(currentBook);
-  const updatedList = myBookList.fetchBooks()
+  const updatedList = myBookList.fetchBooks();
   bookSection.innerHTML = updatedList.join('');
   authorInput.value = '';
   titleInput.value = '';
 });
-
-// const removeBtns = document.querySelectorAll('.btn-remove')
-
-// removeBtns.forEach((btn) => {
-//   console.log(btn)
-//   btn.onclick((e) => {
-//     console.log('hi from obj')
-//     myBookList.removeBook(btn.id)
-//     const updatedList = myBookList.fetchBooks()
-//     console.log(updatedList, btn.id);
-//     bookSection.innerHTML = updatedList.join('');  
-//   })
-// })
